55c0252938e47fb34ab0c5dc761190d93f9831ed
vinod <vinodjy9@gmail.com>
2020-10-27 09:23:42 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './App.css';
+import Header from  './Header';
+import Nav from "./Nav";
+import Result from './Result';
+import requests from "./request";
+
+function App() {
+  const [selectedOption, setSelectedOption]= useState(requests.fetchTrending);
+  return (
+    <div className="app">
+      <Header />
+      <Nav setSelectedOption={setSelectedOption}/>
+      <Result selectedOption={selectedOption}/>
+    </div>
+  );
+}
+
+export default App;
